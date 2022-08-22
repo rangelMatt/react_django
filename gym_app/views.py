ba547dd06aa5a893e682fdb09110b8a32104fcc1
Matt Rangel <mattrangel@gmail.com>
2022-08-22 15:10:03 -0700

    Added paramaters manually

--- gym_app/views.py
+++ gym_app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 
 
 # Create your views here.
 class Another(View):
+
+    book = Book.objects.get(id=1)
+    output = f"We have a {book.title} book with ID {book.id}<br>"
+    # for book in books:
+    #     output += f"We have a {book.title} book with ID {book.id}<br>"
+
     def get(self, request):
-        return HttpResponse('This is another function inside class')
+        return HttpResponse(self.output)
 
 
 def the_gym(request):
