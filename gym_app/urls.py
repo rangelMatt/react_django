2b967402cc92acb2a946498e07bf7b9f7c74078c
Matt Rangel <mattrangel@gmail.com>
2022-08-22 16:02:05 -0700

    made first template

--- gym_app/urls.py
+++ gym_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from .views import Another
 
 urlpatterns = [
     path('the_gym', views.the_gym),
-    path('another', Another.as_view()),
 ]
--- gym_app/views.py
+++ gym_app/views.py
@@ -2,17 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
-
-
-# Create your views here.
-class Another(View):
-
-    book = Book.objects.get(id=1)
-    output = f"We have {book.title} book with ID {book.id}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
+from django.shortcuts import render
 
 
 def the_gym(request):
-    return HttpResponse('First message from views')
+    return render(request, 'first_temp.html')
